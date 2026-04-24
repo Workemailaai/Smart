53377b88540b0e834357bf7b598bbfb2d9c20fee
Pavel-Sk1 <pavel.sk1995@yandex.ru>
2026-04-24 18:55:21 +0400

    PS-1-fix-styles-убрал редирект на список мероприятий принудительный

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -7,9 +7,14 @@ import { useEffect } from 'react'
 import { userStore } from '@/entities/user'
 
 export const App = observer(() => {
-useEffect(() => {
-  void userStore.refreshTokens()
-}, [])
+  useEffect(() => {
+    void userStore.refreshTokens()
+  }, [])
+
+  if (!userStore.isAuthCheckCompleted) {
+    return <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}>Проверка сессии...</div>
+  }
+
   return (
     <>
       <RouterProvider router={routerConfig} />
